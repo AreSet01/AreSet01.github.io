2cbed2e8f1e9c1d3e071d260f9058dda4b584806
AreSet <1909616051@qq.com>
2026-04-10 00:09:59 +0800

    action

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://yourdomain.com',
+  site: 'https://AreSet01.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
